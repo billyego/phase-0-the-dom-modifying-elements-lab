f3e347bdad2bf33df8275cc1b57fbcf6b2cf876c
Bill <bill.kimtai@student.moringaschool.com>
2024-03-29 13:36:19 +0300

    DOM modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Select the "main" element and remove it
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+// Create a new h1 element with desired text content
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion'; // Replace 'YOUR-NAME' with your name
+
+// Append the new h1 element to the body
+document.body.append(newHeader);
